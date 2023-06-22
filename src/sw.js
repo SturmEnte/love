04258cccd2fbcdbbba1d2f4668c3daee837af2ca
Jonas <sturmente@proton.me>
2023-06-22 17:49:25 +0200

    Added offline cache to service worker

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,33 @@
-self.oninstall = (e) => {
-	// Install event
-	console.log("Install", e);
+const STATIC_CACHE = "STATIC-V1";
+
+const ASSETS = ["/", "/style.css", "main.js", "favicon.png", "manifest.webmanifest"];
+
+self.oninstall = async (e) => {
+	cache = await caches.open(STATIC_CACHE);
+	cache.addAll(ASSETS);
 };
 
 self.onfetch = (e) => {
-	// Fetch event
-	console.log("Fetch", e);
+	e.respondWith(
+		caches.match(e.request).then((res) => {
+			return (
+				res ||
+				fetch(e.request)
+					.then(async (res) => {
+						return res;
+					})
+					.catch(() => {
+						// console.log("Catch");
+						// return caches.match("/404/").then((res) => {
+						// 	return res;
+						// });
+					})
+			);
+		})
+	);
 };
 
-self.onactivate = (e) => {
-	// Activate event
-	console.log("Activate", e);
-};
+// self.onactivate = (e) => {
+// 	// Activate event
+// 	console.log("Activate", e);
+// };
